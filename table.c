6e62fca21c417a6c5f1e053d9d451851b1049afe
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2023-07-02 13:07:13 +0530

    table Program

---
+++ table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i=1,n;
+	printf("Enter Number :");
+	scanf("%d",&n);
+	printf("Multiplication Table =");
+	while(i<=10)
+	{
+		printf("\n %d",i*n);
+		i++;
+	}
+		
+}
